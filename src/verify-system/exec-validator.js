52ae795995f5d00dce9cc565cfb304b10bb8f319
Kent C. Dodds <kent@doddsfamily.us>
2019-05-09 14:02:51 -0600

    fix situation for missing binaries

--- src/verify-system/exec-validator.js
+++ src/verify-system/exec-validator.js
@@ -1,11 +1,20 @@
-const {execSync} = require('child_process')
-const semver = require('semver')
+import {execSync} from 'child_process'
+import semver from 'semver'
+import oneLine from '../utils/one-line'
 
 export default execValidator
 
 function execValidator(desired, command, message) {
-  const actual = execSync(command)
-    .toString()
-    .trim()
+  let actual = '0.0.0'
+  try {
+    actual = execSync(command)
+      .toString()
+      .trim()
+  } catch (error) {
+    return oneLine`
+      There was an error running the command \`${command}\`:
+      ${error.message}
+    `
+  }
   return semver.satisfies(actual, desired) ? null : message(actual, desired)
 }
